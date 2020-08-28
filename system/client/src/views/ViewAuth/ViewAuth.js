84fb3bb68ada74c744d7f31a5a51eba15a54048a
Octavian Albu <wu.octavian@gmail.com>
2020-08-28 16:52:04 +0300

    Progres - convertire 'ViewAuth' din SFC in OOP

--- system/client/src/views/ViewAuth/ViewAuth.js
+++ system/client/src/views/ViewAuth/ViewAuth.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import Icon from "@material-ui/core/Icon";
 // @material-ui/icons
@@ -18,117 +18,154 @@ import CardBody from "components/Card/CardBody.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardFooter from "components/Card/CardFooter.js";
 import CustomInput from "components/CustomInput/CustomInput.js";
+import { Link } from "react-router-dom";
 
 import styles from "assets/jss/clear-crm/views/viewAuth.js";
 
 import image from "assets/img/view-auth-bg.png";
-import Link from "@material-ui/core/Link";
 
-const useStyles = makeStyles(styles);
-
-export default function ViewAuth(props) {
-  const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
-  setTimeout(function() {
-    setCardAnimation("");
-  }, 700);
-  const classes = useStyles();
-  const { ...rest } = props;
-  const authCredentials = {
-      email:"abc@gmail.com",
-      password:"abc"
+class ViewAuth extends Component {
+  state = {
+    cardAnimaton: "cardHidden",
+    buttonState: true,
   };
+  render() {
+    const url = this.props.location.pathname;
 
-  const url = props.location.pathname;
+    let loginOrRetrievePasswordURL = url === "/view-auth";
 
-  let showPassword = (url === '/view-auth');
+    const loginText = loginOrRetrievePasswordURL
+      ? "Please type in your credentials"
+      : "Enter your email address";
 
-  const loginText = showPassword ? "Please type in your credentials" : "Enter your email address";
+    const submitButtonText = loginOrRetrievePasswordURL
+      ? "Access account"
+      : "Recover Password";
 
-  const onSubmit = (event) => {
+    const recoverPasswordText = loginOrRetrievePasswordURL
+      ? "Forgot Password ?"
+      : "";
+
+    const onSubmit = (event) => {
       event.preventDefault();
       console.log(event);
       //TODO check against authCredentials
       //TODO check valid email format
       //TODO add error messages
       //TODO
-  }
+    };
 
-  return (
-    <div>
-      {/*<Header
-        absolute
-        color="transparent"
-        brand="Crm System Authentication"
-        rightLinks={<HeaderLinks />}
-        {...rest}
-      />*/}
-      <div
-        className={classes.pageHeader}
-        style={{
-          backgroundImage: "url(" + image + ")",
-          backgroundSize: "cover",
-          backgroundPosition: "center center"
-        }}
-      >
-        <div className={classes.container}>
-          <GridContainer justify="center">
-            <GridItem xs={12} sm={12} md={4}>
-              <Card className={classes[cardAnimaton]}>
-                <form onSubmit={(event) => {onSubmit(event)}} className={classes.form}>
-                  <CardHeader color="primary" className={classes.cardHeader}>
-                    <h4>Crm System Authentication</h4>
-                  </CardHeader>
-                  <p className={classes.divider}>{loginText}</p>
-                  <CardBody>
-                    <CustomInput
-                      labelText="Email"
-                      id="email"
-                      formControlProps={{
-                        fullWidth: true
-                      }}
-                      inputProps={{
-                        type: "username",
-                        endAdornment: (
-                          <InputAdornment position="end">
+    // const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
+    // setTimeout(function () {
+    //   let state1 = [...this.state.cardAnimaton];
+    //   let state2 = "";
+    //   this.setState((state1: state2));
+    // }, 700);
+    const classes = this.props.classes;
+    console.log(classes);
+    const authCredentials = {
+      email: "abc@gmail.com",
+      password: "abc",
+    };
+
+    return (
+      <div>
+        {/*<Header
+          absolute
+          color="transparent"
+          brand="Crm System Authentication"
+          rightLinks={<HeaderLinks />}
+          {...rest}
+        />*/}
+        <div
+          className={classes.pageHeader}
+          style={{
+            backgroundImage: "url(" + image + ")",
+            backgroundSize: "cover",
+            backgroundPosition: "center center",
+          }}
+        >
+          <div className={classes.container}>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={4}>
+                <Card>
+                  <form
+                    onSubmit={(event) => {
+                      onSubmit(event);
+                    }}
+                    className={classes.form}
+                  >
+                    <CardHeader color="primary" className={classes.cardHeader}>
+                      <h4>Crm System Authentication</h4>
+                    </CardHeader>
+                    <p className={classes.divider}>{loginText}</p>
+                    <CardBody>
+                      <CustomInput
+                        labelText="Email"
+                        id="email"
+                        formControlProps={{
+                          fullWidth: true,
+                        }}
+                        inputProps={{
+                          type: "username",
+                          endAdornment: (
+                            <InputAdornment position="end">
                               <Icon className={classes.inputIconsColor}>
-                                  account_circle
+                                account_circle
                               </Icon>
-                          </InputAdornment>
-                        )
-                      }}
-                    />
-                      { showPassword ? <CustomInput
-                      labelText="Password"
-                      id="password"
-                      formControlProps={{
-                        fullWidth: true
-                      }}
-                      inputProps={{
-                        type: "password",
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <Icon className={classes.inputIconsColor}>
-                              lock_outline
-                            </Icon>
-                          </InputAdornment>
-                        ),
-                        autoComplete: "off"
-                      }}
-                    /> : <React.Fragment></React.Fragment>}
-                      <Link className={classes.recoverPassword} target="/recover-password">Forgot password?</Link>
-                  </CardBody>
-                  <CardFooter className={classes.cardFooter}>
-                    <Button type="submit" color="primary" size="lg">
-                      Access account
-                    </Button>
-                  </CardFooter>
-                </form>
-              </Card>
-            </GridItem>
-          </GridContainer>
+                            </InputAdornment>
+                          ),
+                        }}
+                      />
+                      {loginOrRetrievePasswordURL ? (
+                        <CustomInput
+                          labelText="Password"
+                          id="password"
+                          formControlProps={{
+                            fullWidth: true,
+                          }}
+                          inputProps={{
+                            type: "password",
+                            endAdornment: (
+                              <InputAdornment position="end">
+                                <Icon className={classes.inputIconsColor}>
+                                  lock_outline
+                                </Icon>
+                              </InputAdornment>
+                            ),
+                            autoComplete: "off",
+                          }}
+                        />
+                      ) : (
+                        <React.Fragment></React.Fragment>
+                      )}
+                      <Link
+                        className={classes.recoverPassword}
+                        to="/recover-password"
+                      >
+                        {recoverPasswordText}
+                      </Link>
+                    </CardBody>
+                    <CardFooter className={classes.cardFooter}>
+                      <Button
+                        disabled={!this.state.buttonState}
+                        type="submit"
+                        color="primary"
+                        size="lg"
+                      >
+                        {submitButtonText}
+                      </Button>
+                    </CardFooter>
+                  </form>
+                </Card>
+              </GridItem>
+            </GridContainer>
+          </div>
+          {/*<Footer whiteFont />*/}
         </div>
-        {/*<Footer whiteFont />*/}
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default withStyles(styles)(ViewAuth);
