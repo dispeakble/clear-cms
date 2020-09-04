11c817dcf218078f34ac28aa35209d2fc564e02f
Octavian Albu <wu.octavian@gmail.com>
2020-09-04 14:18:33 +0300

    git testing

--- system/client/src/views/ProfilePage/ProfilePage.js
+++ system/client/src/views/ProfilePage/ProfilePage.js
@@ -580,7 +580,7 @@ class ProfilePage extends Component {
                             size="lg"
                             className={classes.button}
                           >
-                            Save
+                            Save settings
                           </Button>
 
                           <h3>Link Accounts</h3>
