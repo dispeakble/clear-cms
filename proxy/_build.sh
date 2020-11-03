223acaf1a5dd97f4f27830a60962b589192f66f8
Ovidiu Alexa <ovidiu.alexa@gmail.com>
2020-11-03 21:44:04 +0000

    DOSIDO-52 deployments and fixes

--- proxy/_build.sh
+++ proxy/_build.sh
@@ -1,9 +1,2 @@
-#!/bin/sh -ex
-# tip: do..
-# DEV_IMAGE_TAG=$(date +%s) ./_build.sh
-#...to tag image with linux timestamp
-DEV_IMAGE_TAG=$(date +%s) # default DEV_IMAGE_TAG
-IMAGE_NAME_TAG="dispeakble/crm-hub:${DEV_IMAGE_TAG}"
-
-docker build -t "${IMAGE_NAME_TAG}" .
-docker push IMAGE_NAME_TAG
+#!/usr/bin/env bash
+docker build . -t dispeakble/crm-proxy && docker push dispeakble/crm-proxy
---
+++ system/_build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -ex
+# tip: do..
+# DEV_IMAGE_TAG=$(date +%s) ./_build.sh
+#...to tag image with linux timestamp
+DEV_IMAGE_TAG=$(date +%s) # default DEV_IMAGE_TAG
+IMAGE_NAME_TAG="dispeakble/crm-system:${DEV_IMAGE_TAG}"
+
+docker build -t "${IMAGE_NAME_TAG}" .
+docker push IMAGE_NAME_TAG
