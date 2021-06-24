7a6361a2ab3f2820c376fbebdcd3219cbfd638aa
Ovidiu Alexa <ovidiu.alexa@gmail.com>
2021-06-24 22:58:42 +0100

    deployment upgrade. DB schema. teaks

--- deploy/pg.db/db.schema.sql
+++ deploy/pg.db/db.schema.sql
@@ -7,6 +7,15 @@
 
 -- Started on 2021-02-17 21:53:05 UTC
 
+CREATE ROLE cms WITH
+  LOGIN
+  NOSUPERUSER
+  INHERIT
+  CREATEDB
+  NOCREATEROLE
+  NOREPLICATION
+  ENCRYPTED PASSWORD 'md5435aae228b373a0313b9d3b45756ab18';
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -22,6 +31,14 @@ SET row_security = off;
 -- TOC entry 196 (class 1259 OID 16451)
 -- Name: admin_themes_id_seq; Type: SEQUENCE; Schema: public; Owner: cms
 --
+
+CREATE DATABASE cms
+    WITH
+    OWNER = cms
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    CONNECTION LIMIT = -1;
 
 CREATE SEQUENCE public.admin_themes_id_seq
     START WITH 1
@@ -546,3 +563,6 @@ CREATE UNIQUE INDEX email_idx ON public.auth_admin USING btree (email COLLATE "C
 -- PostgreSQL database dump complete
 --
 
+INSERT INTO public.auth_admin(
+	email, fullname, fname, lname, password, active)
+	VALUES ('ovidiu.alexa@gmail.com', 'Ovidiu Alexa', 'Ovidiu', 'Alexa', '1qaz', 1);
