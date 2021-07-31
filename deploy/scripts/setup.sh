d2bee7a75745f8e6cfceb2ba9e856e427c02123d
Ovidiu Alexa <ovidiu.alexa@gmail.com>
2021-07-31 19:45:11 +0100

    pass changed on bitbucket repo

--- deploy/scripts/setup.sh
+++ deploy/scripts/setup.sh
@@ -34,7 +34,7 @@ fi
 DOCKERHUB_USERNAME="cmsbot"
 DOCKERHUB_PASS="@u*82fmVP^GVx2$"
 BITBUCKET_USERNAME="the_dispeakble_one"
-BITBUCKET_PASS="He@cmkGN7q^cl2n"
+BITBUCKET_PASS='0$$2Dbmc!Hv7zpA'
 
 #DOCKERHUB_CRED_FILE=./dockerhub_credentials.txt
 #if [[ -f "$DOCKERHUB_CRED_FILE" ]]; then
