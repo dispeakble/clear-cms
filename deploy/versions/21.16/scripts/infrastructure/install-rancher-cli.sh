3799ee2767ed9f03ef21c8f3a781b14a62c2c7b4
Ovidiu Alexa <ovidiu.alexa@gmail.com>
2021-08-18 15:51:57 +0100

    new deploy scripts added and removed cluster 1.0.0

---
+++ deploy/versions/21.16/scripts/infrastructure/install-rancher-cli.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+RANCHER_CLI="false"
+RANCHER_CLI_VERSION="v2.4.11"
+CMS_repo_rancher_cli="https://github.com/rancher/cli/releases/download/$RANCHER_CLI_VERSION/rancher-linux-amd64-$RANCHER_CLI_VERSION.tar.xz"
+CMS_file_rancher_cli="rancher-linux-amd64-$RANCHER_CLI_VERSION.tar.xz"
+
+function install_rancher_cli() {
+    if ! dpkg -s "$1" >/dev/null 2>&1; then
+        echo "started install $1"
+        apt-get --assume-yes -y install "$1"
+    fi
+    if ! which rancher >/dev/null; then
+        pushd "$(mktemp -d)"
+        wget "$CMS_repo_rancher_cli"
+        tar xf "$CMS_file_rancher_cli"
+        mv rancher-*/rancher /usr/bin/
+        rm -rf rancher-*
+        popd
+    fi
+}
+
+install_rancher_cli jq
---
+++ deploy/versions/21.16/scripts/setup_with_cluster.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if ((EUID != 0)); then
+    echo "try sudo $0 $@"
+    exit
+fi
+
+params="$@"
+
+installService() {
+  chmod +x $1
+  echo
+  echo "install: $1"
+  source "./$1"
+  if [ $? != 0 ]; then
+      echo "error while trying to run: $1"
+      exit 1
+  fi
+}
+if [ -z "$(command -v curl)" ]; then
+  sudo apt-get -y install curl
+fi
+
+if [ -z "$(command -v docker)" ]; then
+  installService "infrastructure/install-docker.sh"
+fi
+if [ -z "$(command -v helm)" ]; then
+  installService "infrastructure/install-helm.sh"
+fi
+if [ -z "$(command -v kubectl)" ]; then
+  installService "infrastructure/install-kubectl.sh"
+fi
+
+DOCKERHUB_USERNAME="cmsbot"
+DOCKERHUB_PASS="yTazvEWhtEbk4mv"
+BITBUCKET_USERNAME="the_dispeakble_one"
+BITBUCKET_PASS="He@cmkGN7q^cl2n"
+
+sudp apt update
+sudo apt upgrade -y
+
+installService "cluster/launch_with_cluster.sh"
+
+if [ $? != 0 ]; then
+   echo
+   echo "failed to install"
+   exit 1
+fi
+
+exit
