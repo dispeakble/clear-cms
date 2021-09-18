a3b57611e9d4c0088686bc396cf162b5501661ae
Ovidiu Alexa <ovidiu.alexa@gmail.com>
2021-09-18 21:30:08 +0100

    transfer progress to desktop

--- core/frontend/api/src/server/app.module.ts
+++ core/frontend/api/src/server/app.module.ts
@@ -1,10 +1,42 @@
 import { Module } from '@nestjs/common';
-
-import { ViewModule } from './modules/view/view.module';
+import { ProtocolController } from './controllers/protocol.controller';
+import { BucketService } from './services/bucket.service';
+import { CategoriesService } from './services/categories.service';
+import { PagesService } from './services/pages.service';
+import { PublicThemesService } from './services/publicThemes.service';
+import {
+  ClientsModule, Transport,
+} from '@nestjs/microservices';
+import {ProtocolService} from "./services/protocol.service";
+import {GotModule} from "@t00nday/nestjs-got";
+import { MainService } from './services/main.service';
 
 @Module({
-  imports: [ViewModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    GotModule.register(),
+    ClientsModule.register([
+      {
+        name: 'REDIS_SERVICE',
+        transport: Transport.REDIS,
+        options: {
+          url: 'redis://' + process.env.redis_server,
+          port: +process.env.redis_port,
+          password: process.env.redis_password,
+          retryAttempts: 20,
+          retryDelay: 3000,
+        }
+      },
+    ])
+  ],
+  controllers: [ProtocolController],
+  providers: [
+    BucketService,
+    CategoriesService,
+    PagesService,
+    ProtocolService,
+    PublicThemesService,
+    MainService
+  ]
 })
+
 export class AppModule {}
--- core/frontend/api/src/server/services/view.service.ts
+++ core/frontend/api/src/server/services/view.service.ts
@@ -1,4 +1,4 @@
-import {Injectable, OnModuleInit} from '@nestjs/common';
+import {Inject, Injectable, OnModuleInit} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import createServer from 'next';
 import { NextServer } from 'next/dist/server/next';
@@ -9,7 +9,7 @@ import { Request, Response } from 'express';
 export class ViewService implements OnModuleInit {
   private server: NextServer;
 
-  constructor(private configService: ConfigService) {}
+  constructor(private configService: ConfigService, @Inject('ProtocolService') private protocolService) {}
 
   async onModuleInit(): Promise<void> {
     try {
@@ -26,6 +26,9 @@ export class ViewService implements OnModuleInit {
   }
 
   handler(req: Request, res: Response) {
+
+
+
     return this.server.getRequestHandler()(req, res);
   }
 }
