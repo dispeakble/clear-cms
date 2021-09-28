8580a1c1c8172876dca0518acca91086c1cda0ec
Ovidiu Alexa <ovidiu.alexa@gmail.com>
2021-09-28 16:20:37 +0100

    removed hard coded border from boxes

--- core/system/client/src/assets/jss/clear-crm/views/pagePreview.js
+++ core/system/client/src/assets/jss/clear-crm/views/pagePreview.js
@@ -12,7 +12,6 @@ const javascriptStyles = {
     top: 0,
     left: 0,
     "& .react-grid-item": {
-      border: "1px solid rgba(0,0,0,0.14)",
       boxSizing: "border-box",
     },
     "& .react-grid-placeholder": {},
