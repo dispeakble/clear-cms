85509e8392c88ee60d39725e07c5c60cb6eee904
Ovidiu Alexa <ovidiu.alexa@gmail.com>
2021-10-16 16:02:50 +0100

    deploy fixes

--- core/hub/api/src/main.ts
+++ core/hub/api/src/main.ts
@@ -1,27 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
-import {MicroserviceOptions, Transport} from '@nestjs/microservices';
+import {Transport} from '@nestjs/microservices';
 
 Logger.overrideLogger(['error']);
 
 async function bootstrap() {
-    const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-        transport: Transport.REDIS,
-        options: {
-            url: 'redis://' + process.env.redis_server,
-            port: +process.env.redis_port,
-            password: process.env.redis_password,
-            retryAttempts: 10,
-            retryDelay: 5000,
-            disable_resubscribing: false,
-            max_attempts: 30,
-            no_ready_check: true,
-            retry_max_delay: 1000,
-            retry_strategy: 1000
-        },
-    });
-    await app.listen(() => console.log('hub is ready.'));
+    try {
+        const app = await NestFactory.create(
+            AppModule
+        );
+
+        await app.init();
+
+        await app.connectMicroservice({
+            transport: Transport.REDIS,
+            options: {
+                url: 'redis://' + process.env.redis_server,
+                port: +process.env.redis_port,
+                password: process.env.redis_password,
+                retryAttempts: 20,
+                retryDelay: 5000,
+                disable_resubscribing: false,
+                max_attempts: 30,
+                no_ready_check: true,
+                detect_buffers: true,
+                retry_max_delay: 1000,
+                retry_strategy: 1000
+            }
+        });
+
+        await app.startAllMicroservicesAsync();
+    } catch(err) {
+        console.error(err);
+        process.exit(1);
+    }
+
 
 }
 bootstrap();
--- plugins/dosido/db/api/src/main.ts
+++ plugins/dosido/db/api/src/main.ts
@@ -4,20 +4,22 @@ import { Logger } from '@nestjs/common';
 import { Transport } from '@nestjs/microservices';
 
 Logger.overrideLogger(['error']);
-// Create a logger instance
-const logger = new Logger('Db');
 
 async function bootstrap() {
     try {
-        const app = await NestFactory.createMicroservice(AppModule, {
+        const app = await NestFactory.create(
+            AppModule
+        );
+        await app.init();
+        await app.connectMicroservice({
             transport: Transport.REDIS,
             options: {
                 url:  'redis://' + process.env.redis_server,
                 port: +process.env.redis_port,
                 password: process.env.redis_password,
                 retryAttempts: 20,
-                retryDelay: 3000,
-                return_buffers: true,
+                retryDelay: 5000,
+                detect_buffers: true,
                 disable_resubscribing: false,
                 max_attempts: 30,
                 no_ready_check: true,
@@ -25,9 +27,11 @@ async function bootstrap() {
                 retry_strategy: 1000
             }
         });
-        await app.listen(() => console.log('db started.', ...arguments));
-    } catch(e){
-        logger.log('Warning! Could not start event listener');
+        await app.startAllMicroservicesAsync();
+    } catch(err){
+        console.error(err);
+        process.exit(1);
     }
+
 }
 bootstrap();
