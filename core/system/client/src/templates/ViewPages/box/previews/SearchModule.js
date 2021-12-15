91bf46a7a4512b00db35fa63e6eb660238e65f3f
ahmedzrouqui <zrouqui.ahmed.Az@gmail.com>
2021-12-15 13:12:04 +0100

    upgrade: nextjs 12

--- core/system/client/src/templates/ViewPages/box/previews/SearchModule.js
+++ core/system/client/src/templates/ViewPages/box/previews/SearchModule.js
@@ -1,5 +1,7 @@
 import React, { Component, createRef } from "react";
 import {Link} from "react-router-dom";
+import {ReactComponent as SearchIcon} from "../../../../assets/icons/search.svg"
+import {ReactComponent as ClearIcon} from "../../../../assets/icons/clear.svg"
 
 class SearchModule extends Component {
     state = {
@@ -104,11 +106,7 @@ class SearchModule extends Component {
                     <div className="clear-crm_search-bar">
                         <div className="clear-crm_search-bar--search">
                             <div className="search-icon">
-                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129" enableBackground="new 0 0 129 129">
-                                    <g>
-                                        <path d="M51.6,96.7c11,0,21-3.9,28.8-10.5l35,35c0.8,0.8,1.8,1.2,2.9,1.2s2.1-0.4,2.9-1.2c1.6-1.6,1.6-4.2,0-5.8l-35-35   c6.5-7.8,10.5-17.9,10.5-28.8c0-24.9-20.2-45.1-45.1-45.1C26.8,6.5,6.5,26.8,6.5,51.6C6.5,76.5,26.8,96.7,51.6,96.7z M51.6,14.7   c20.4,0,36.9,16.6,36.9,36.9C88.5,72,72,88.5,51.6,88.5c-20.4,0-36.9-16.6-36.9-36.9C14.7,31.3,31.3,14.7,51.6,14.7z"/>
-                                    </g>
-                                </svg>
+                                <SearchIcon />
                             </div>
                             <div className="search-input">
                                 <form style={{height: '100%'}}>
@@ -135,16 +133,7 @@ class SearchModule extends Component {
 
                             </div>
                             <div className="search-clear" ref={this.clearSearchBtn} onClick={this.clearSearch}>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
-                                     stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                                     className="feather feather-x">
-                                    <line x1="18" y1="6" x2="6" y2="18">
-                                    </line>
-                                    <line x1="6"
-                                    y1="6"
-                                    x2="18"
-                                    y2="18"></line>
-                                </svg>
+                                <ClearIcon />
                             </div>
                         </div>
                     </div>
