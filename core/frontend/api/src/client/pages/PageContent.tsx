077c1802159947650426d296362ea384ac0c2016
ahmedzrouqui <zrouqui.ahmed.Az@gmail.com>
2021-12-21 15:09:25 +0100

    fix: remove console log

--- core/frontend/api/src/client/pages/PageContent.tsx
+++ core/frontend/api/src/client/pages/PageContent.tsx
@@ -95,8 +95,6 @@ class PageContent{
         const {pageMetaTitle, pageMetaDescription, pageFavicon} = JSON.parse(props.pageData.pageConfig.data) ||
             { pageMetaTitle: null, pageMetaDescription: null, pageFavicon: null}
 
-        console.log(JSON.parse(props.pageData.pageConfig.data))
-
 
         return(
             <>
