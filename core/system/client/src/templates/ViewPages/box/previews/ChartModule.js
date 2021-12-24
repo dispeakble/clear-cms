ccf2bf78ea0c4349fc252582f3438af51524a086
Utkarsh Siddharth <utsiddharth@gmail.com>
2021-12-25 00:37:20 +0530

    chart module fixed - https://dosidoweb.atlassian.net/browse/DOSIDO-314

--- core/system/client/src/templates/ViewPages/box/previews/ChartModule.js
+++ core/system/client/src/templates/ViewPages/box/previews/ChartModule.js
@@ -1,22 +1,38 @@
 import React, { Component } from "react";
 import Chart from "react-google-charts";
+import PropTypes from "prop-types";
 
 class ChartModule extends Component {
-  state = { chartType: "", data: { rows: "", columns: "" } };
+  state = { chartType: "", data: [] };
+
+  componentDidMount(){
+      const {moduleOptions} = this.props;
+      this.setState({
+          chartTitle: moduleOptions.chartTitle || '',
+          chartType: moduleOptions.chartType || '',
+          data: moduleOptions.data || [],
+      });
+  }
+
   render() {
     return (
       <div style={{ width: "100%" }}>
+          <h4>{this.state.chartTitle}</h4>
         <Chart
           chartType={this.state.chartType}
           width="100%"
           height="400px"
           legendToggle
-          rows={this.state.data.rows}
-          columns={this.state.data.columns}
+          data={this.state.data.rows}
         />
       </div>
     );
   }
+}
+
+ChartModule.propTypes = {
+    onUpdate: PropTypes.func,
+    moduleOptions: PropTypes.object,
 }
 
 export default ChartModule;
