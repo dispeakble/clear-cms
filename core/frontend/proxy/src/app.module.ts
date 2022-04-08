61adaba3cccb5e5de69daeb292c34cc7220666a0
Ovidiu Alexa <ovidiu.alexa@gmail.com>
2022-04-08 22:19:05 +0100

    Testing 7 (#222)
    * testing frontend proxy CI
    
    * testing frontend proxy CI
    
    * testing frontend proxy CI

--- core/frontend/proxy/src/app.module.ts
+++ core/frontend/proxy/src/app.module.ts
@@ -1,25 +1,25 @@
-import {Module, CacheModule, Logger} from '@nestjs/common';
-import {AppController} from "./controllers/app.controller";
-import {AppService} from './services/app.service';
-import {ProtocolService} from "./services/protocol.service";
-import { WsGateway } from './gateways/ws.gateway';
-import {SystemService} from "./services/system.service";
-import {ClientsModule, Transport} from "@nestjs/microservices";
-import { Session } from './modules/session.module';
-import * as redisStore from 'cache-manager-redis-store';
-import {SessionService} from "./services/session.service";
-import {ConfigService} from "./services/config.service";
-import { GotModule, GotModuleOptions } from '@t00nday/nestjs-got';
+import { CacheModule, Logger, Module } from "@nestjs/common";
+import { AppController } from "./controllers/app.controller";
+import { AppService } from "./services/app.service";
+import { ProtocolService } from "./services/protocol.service";
+import { WsGateway } from "./gateways/ws.gateway";
+import { SystemService } from "./services/system.service";
+import { ClientsModule, Transport } from "@nestjs/microservices";
+import { Session } from "./modules/session.module";
+import * as redisStore from "cache-manager-redis-store";
+import { SessionService } from "./services/session.service";
+import { ConfigService } from "./services/config.service";
+import { GotModule, GotModuleOptions } from "@t00nday/nestjs-got";
 
 
 @Module({
     imports: [
         GotModule.registerAsync({
-            useFactory: (): GotModuleOptions => ({}),
+            useFactory: (): GotModuleOptions => ({})
         }),
         CacheModule.register({
             store: redisStore,
-            url: 'redis://' + process.env.redis_server,
+            url: "redis://" + process.env.redis_server,
             port: +process.env.redis_port,
             password: process.env.redis_password,
             retryAttempts: 20,
@@ -33,10 +33,10 @@ import { GotModule, GotModuleOptions } from '@t00nday/nestjs-got';
         Session,
         ClientsModule.register([
             {
-                name: 'REDIS_SERVICE',
+                name: "REDIS_SERVICE",
                 transport: Transport.REDIS,
                 options: {
-                    url: 'redis://' + process.env.redis_server,
+                    url: "redis://" + process.env.redis_server,
                     port: +process.env.redis_port,
                     password: process.env.redis_password,
                     retryAttempts: 20,
@@ -47,7 +47,7 @@ import { GotModule, GotModuleOptions } from '@t00nday/nestjs-got';
                     retry_max_delay: 1000,
                     retry_strategy: 1000
                 }
-            },
+            }
         ])
     ],
     controllers: [AppController],
