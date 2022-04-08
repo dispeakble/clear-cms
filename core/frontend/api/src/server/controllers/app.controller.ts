f4790e54ef0ce7e13c7279e7480b9ab2cbd18311
Ovidiu Alexa <ovidiu.alexa@gmail.com>
2022-04-08 21:35:58 +0100

    testing frontend CI

--- core/frontend/api/src/server/controllers/app.controller.ts
+++ core/frontend/api/src/server/controllers/app.controller.ts
@@ -229,11 +229,11 @@ export class AppController {
         const dummy = [[
             {
             itemTitle: "Lorem ipsum - page1",
-            itemDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            itemDescription: "Lorem ipsum dolor sit amet,adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
             },
             {
                 itemTitle: "Lorem ipsum",
-                itemDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                itemDescription: "Lorem ipsum dolor sit amet, adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
             },
             {
                 itemTitle: "Lorem ipsum",
