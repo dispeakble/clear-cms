42b72527af35d6dcf974daf2552f651bef202298
sufyankhatri <sufyankhatree@gmail.com>
2022-05-23 19:47:41 +0500

    removed laptopM

--- core/frontend/api/src/client/templates/v1/components/HotelResultsContent/styled.tsx
+++ core/frontend/api/src/client/templates/v1/components/HotelResultsContent/styled.tsx
@@ -7,7 +7,7 @@ export const Wrapper = styled.div`
       width: 100%;
     }
     flex: 0 1 70%;
-  @media (max-width: ${size.laptopM}) {
+  @media (max-width: ${size.laptopL}) {
     flex: 1 0 100%;
     min-width: 0px;
   }
