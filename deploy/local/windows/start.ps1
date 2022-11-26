400d5e2af9c337dac7cbd85ddfb3cdafae50e7fe
Ovidiu Alexa <ovidiu.alexa@gmail.com>
2022-11-26 14:44:04 +0000

    windows powershell refactor. added optional variable prompts

--- deploy/local/windows/start.ps1
+++ deploy/local/windows/start.ps1
@@ -1,57 +1,64 @@
 #!/usr/bin/env powershell
-$INIT_INSTALL_VERSION="v0.0.1"
-$INIT_APP_NAME = "marioviajes"
-$INIT_APP_VERSION = "1.5.0"
-$INIT_WEBSITE_NAME = "Mario Viajes"
-$INIT_WEBSITE_EMAIL = "contact@marioviajes.com"
-$INIT_ADMIN_FNAME = "Mario"
-$INIT_ADMIN_LNAME = "Viajes"
-$INIT_WEBSITE_DOMAIN = "marioviajes.com"
-$INIT_DEFAULT_EMAIL = "admin@example.com"
-$INIT_DEFAULT_PASSWORD = "1qaz"
-$INIT_TPL_VER = "v1"
-$INIT_CMS_JWT_EXP_H="3600s"
-$INIT_recaptcha_public_key="6LdNqsESAAAAAPHt1ZG6vr4RBQHoM-eVcE4XB4wJ"
-$INIT_recaptcha_private_key="6LdNqsESAAAAANbWPLowt17zjEwNS4MuvVLfX7TQ"
-$INIT_POSTGRES_DEFAULT_USER = "cms"
-$INIT_POSTGRES_DEFAULT_PASSWORD = "1qaz"
-$INIT_POSTGRES_DEFAULT_DB = "main"
-$INIT_POSTGRES_DEFAULT_CONNECTIONS = "main,agency"
-$INIT_PGADMIN_DEFAULT_EMAIL = "admin@example.com"
-$INIT_PGADMIN_DEFAULT_PASSWORD = "1qaz"
-$INIT_REDIS_DEFAULT_PASSWORD = "1gzHwbgfwR"
-$CMS_HUB_VERSION="1660327431"
-$CMS_DB_VERSION="1660327431"
-$CMS_BUCKET_VERSION="1660327431"
-$CMS_FRONTEND_PROXY_VERSION="1660327431"
-$CMS_FRONTEND_VERSION="1660327431"
-$CMS_ADMIN_PROXY_VERSION="1660327431"
-$CMS_ADMIN_SYSTEM_VERSION="1660327431"
-$PWD=$pwd.Path
+Param ([Boolean]$custom = 0)
 
-if (($INSTALL_VERSION = Read-Host "Install version (Enter for default:  $INIT_INSTALL_VERSION)") -eq '') { $INSTALL_VERSION = $INIT_INSTALL_VERSION }
-if (($APP_NAME = Read-Host "App name (Enter for default:  $INIT_APP_NAME)") -eq '') { $APP_NAME = $INIT_APP_NAME }
-if (($APP_VERSION = Read-Host "App version (Enter for default:  $INIT_APP_VERSION)") -eq '') { $APP_VERSION = $INIT_APP_VERSION }
-if (($DEFAULT_EMAIL = Read-Host "Admin Email (Enter for default: $INIT_DEFAULT_EMAIL)") -eq '') { $DEFAULT_EMAIL = $INIT_DEFAULT_EMAIL }
-if (($DEFAULT_PASSWORD = Read-Host "Admin Password (Enter for default: $INIT_DEFAULT_PASSWORD)") -eq '') { $DEFAULT_PASSWORD = $INIT_DEFAULT_PASSWORD }
-if (($WEBSITE_NAME = Read-Host "Website name (Enter for default: $INIT_WEBSITE_NAME)") -eq '') { $WEBSITE_NAME = $INIT_WEBSITE_NAME }
-if (($WEBSITE_EMAIL = Read-Host "Website email (Enter for default: $INIT_WEBSITE_EMAIL)") -eq '') { $WEBSITE_EMAIL = $INIT_WEBSITE_EMAIL }
-if (($ADMIN_FNAME = Read-Host "Admin first name (Enter for default: $INIT_ADMIN_FNAME)") -eq '') { $ADMIN_FNAME = $INIT_ADMIN_FNAME }
-if (($ADMIN_LNAME = Read-Host "Admin last name (Enter for default: $INIT_ADMIN_LNAME)") -eq '') { $ADMIN_LNAME = $INIT_ADMIN_LNAME }
-if (($WEBSITE_DOMAIN = Read-Host "Website domain (Enter for default: $INIT_WEBSITE_DOMAIN)") -eq '') { $WEBSITE_DOMAIN = $INIT_WEBSITE_DOMAIN }
-if (($TPL_VER = Read-Host "App template E.G. v1 (Enter for default:  $INIT_TPL_VER)") -eq '') { $TPL_VER = $INIT_TPL_VER }
-if (($CMS_JWT_EXP_H = Read-Host "JWT expire time (Enter for default:  $CMS_JWT_EXP_H)") -eq '') { $CMS_JWT_EXP_H = $INIT_CMS_JWT_EXP_H }
-if (($recaptcha_public_key = Read-Host "Next JS public recaptcha key (Enter for default:  $INIT_recaptcha_public_key)") -eq '') { $recaptcha_public_key = $INIT_recaptcha_public_key }
-if (($recaptcha_private_key = Read-Host "Recaptcha secret key (Enter for default:  $INIT_recaptcha_private_key)") -eq '') { $recaptcha_private_key = $INIT_recaptcha_private_key }
+$INSTALL_VERSION="v0.0.1"
+$APP_NAME = "marioviajes"
+$APP_VERSION = "1.5.0"
+$WEBSITE_NAME = "Mario Viajes"
+$WEBSITE_EMAIL = "contact@marioviajes.com"
+$ADMIN_FNAME = "Mario"
+$ADMIN_LNAME = "Viajes"
+$WEBSITE_DOMAIN = "marioviajes.com"
+$DEFAULT_EMAIL = "admin@example.com"
+$DEFAULT_PASSWORD = "1qaz"
+$TPL_VER = "v1"
+$CMS_JWT_EXP_H="3600s"
+$recaptcha_public_key="6LdNqsESAAAAAPHt1ZG6vr4RBQHoM-eVcE4XB4wJ"
+$recaptcha_private_key="6LdNqsESAAAAANbWPLowt17zjEwNS4MuvVLfX7TQ"
+$POSTGRES_DEFAULT_USER = "cms"
+$POSTGRES_DEFAULT_PASSWORD = "1qaz"
+$POSTGRES_DEFAULT_DB = "main"
+$POSTGRES_DEFAULT_CONNECTIONS = "main,agency"
+$PGADMIN_DEFAULT_EMAIL = "admin@example.com"
+$PGADMIN_DEFAULT_PASSWORD = "1qaz"
+$REDIS_DEFAULT_PASSWORD = "1gzHwbgfwR"
+$CMS_HUB_VERSION="1669457129"
+$CMS_DB_VERSION="1669457129"
+$CMS_BUCKET_VERSION="1669457129"
+$CMS_FRONTEND_PROXY_VERSION="1669457129"
+$CMS_FRONTEND_VERSION="1669457129"
+$CMS_ADMIN_PROXY_VERSION="1669457129"
+$CMS_ADMIN_SYSTEM_VERSION="1669457129"
+$PWD=Get-Location
 
-if (($REDIS_DEFAULT_PASSWORD = Read-Host "Redis password (Enter for default:  $INIT_REDIS_DEFAULT_PASSWORD)") -eq '') { $REDIS_DEFAULT_PASSWORD = $INIT_REDIS_DEFAULT_PASSWORD }
-if (($POSTGRES_DEFAULT_USER = Read-Host "Postgres user (Enter for default:  $INIT_POSTGRES_DEFAULT_USER)") -eq '') { $POSTGRES_DEFAULT_USER = $INIT_POSTGRES_DEFAULT_USER }
-if (($POSTGRES_DEFAULT_PASSWORD = Read-Host "Postgres password (Enter for default: $INIT_POSTGRES_DEFAULT_PASSWORD)") -eq '') { $POSTGRES_DEFAULT_PASSWORD = $INIT_POSTGRES_DEFAULT_PASSWORD }
-if (($POSTGRES_DEFAULT_DB = Read-Host "Postgres Database name (Enter for default: $INIT_POSTGRES_DEFAULT_DB)") -eq '') { $POSTGRES_DEFAULT_DB = $INIT_POSTGRES_DEFAULT_DB }
-if (($POSTGRES_DEFAULT_CONNECTIONS = Read-Host "Postgres Databases (Enter for default: $INIT_POSTGRES_DEFAULT_CONNECTIONS)") -eq '') { $POSTGRES_DEFAULT_CONNECTIONS = $INIT_POSTGRES_DEFAULT_CONNECTIONS }
 
-if (($PGADMIN_DEFAULT_EMAIL = Read-Host "PgAdmin email (Enter for default: $INIT_PGADMIN_DEFAULT_EMAIL)") -eq '') { $PGADMIN_DEFAULT_EMAIL = $INIT_PGADMIN_DEFAULT_EMAIL }
-if (($PGADMIN_DEFAULT_PASSWORD = Read-Host "PgAdmin password (Enter for default: $INIT_PGADMIN_DEFAULT_PASSWORD)") -eq '') { $PGADMIN_DEFAULT_PASSWORD = $INIT_PGADMIN_DEFAULT_PASSWORD }
+
+if($custom) {
+    echo "Will do a custom build: "
+    if (($user_input = Read-Host "Install version (Enter for default:  $INIT_INSTALL_VERSION)") -ne '') { $INSTALL_VERSION = $user_input}
+    if (($user_input = Read-Host "App name (Enter for default:  $INIT_APP_NAME)") -ne '') { $APP_NAME = $user_input}
+    if (($user_input = Read-Host "App version (Enter for default:  $INIT_APP_VERSION)") -ne '') { $APP_VERSION = $user_input}
+    if (($user_input = Read-Host "Admin Email (Enter for default: $INIT_DEFAULT_EMAIL)") -ne '') { $DEFAULT_EMAIL = $user_input}
+    if (($user_input = Read-Host "Admin Password (Enter for default: $INIT_DEFAULT_PASSWORD)") -ne '') { $DEFAULT_PASSWORD = $user_input}
+    if (($user_input = Read-Host "Website name (Enter for default: $INIT_WEBSITE_NAME)") -ne '') { $WEBSITE_NAME = $user_input}
+    if (($user_input = Read-Host "Website email (Enter for default: $INIT_WEBSITE_EMAIL)") -ne '') { $WEBSITE_EMAIL = $user_input}
+    if (($user_input = Read-Host "Admin first name (Enter for default: $INIT_ADMIN_FNAME)") -ne '') { $ADMIN_FNAME = $user_input}
+    if (($user_input = Read-Host "Admin last name (Enter for default: $INIT_ADMIN_LNAME)") -ne '') { $ADMIN_LNAME = $user_input}
+    if (($user_input = Read-Host "Website domain (Enter for default: $INIT_WEBSITE_DOMAIN)") -ne '') { $WEBSITE_DOMAIN = $user_input}
+    if (($user_input = Read-Host "App template E.G. v1 (Enter for default:  $INIT_TPL_VER)") -ne '') { $TPL_VER = $user_input}
+    if (($user_input = Read-Host "JWT expire time (Enter for default:  $CMS_JWT_EXP_H)") -ne '') { $CMS_JWT_EXP_H = $user_input}
+    if (($user_input = Read-Host "Next JS public recaptcha key (Enter for default:  $INIT_recaptcha_public_key)") -ne '') { $recaptcha_public_key = $user_input}
+    if (($user_input = Read-Host "Recaptcha secret key (Enter for default:  $INIT_recaptcha_private_key)") -ne '') { $recaptcha_private_key = $user_input}
+
+    if (($user_input = Read-Host "Redis password (Enter for default:  $INIT_REDIS_DEFAULT_PASSWORD)") -ne '') { $REDIS_DEFAULT_PASSWORD = $user_input}
+    if (($user_input = Read-Host "Postgres user (Enter for default:  $INIT_POSTGRES_DEFAULT_USER)") -ne '') { $POSTGRES_DEFAULT_USER = $user_input}
+    if (($user_input = Read-Host "Postgres password (Enter for default: $INIT_POSTGRES_DEFAULT_PASSWORD)") -ne '') { $POSTGRES_DEFAULT_PASSWORD = $user_input}
+    if (($user_input = Read-Host "Postgres Database name (Enter for default: $INIT_POSTGRES_DEFAULT_DB)") -ne '') { $POSTGRES_DEFAULT_DB = $user_input}
+    if (($user_input = Read-Host "Postgres Databases (Enter for default: $INIT_POSTGRES_DEFAULT_CONNECTIONS)") -ne '') { $POSTGRES_DEFAULT_CONNECTIONS = $user_input}
+
+    if (($user_input = Read-Host "PgAdmin email (Enter for default: $INIT_PGADMIN_DEFAULT_EMAIL)") -ne '') { $PGADMIN_DEFAULT_EMAIL = $user_input}
+    if (($user_input = Read-Host "PgAdmin password (Enter for default: $INIT_PGADMIN_DEFAULT_PASSWORD)") -ne '') { $PGADMIN_DEFAULT_PASSWORD = $user_input}
+}
 
 $env:APP_NAME=$APP_NAME
 $env:INSTALL_VERSION=$INSTALL_VERSION
@@ -87,7 +94,6 @@ $env:CMS_FRONTEND_VERSION=$CMS_FRONTEND_VERSION
 $env:CMS_ADMIN_PROXY_VERSION=$CMS_ADMIN_PROXY_VERSION
 $env:CMS_ADMIN_SYSTEM_VERSION=$CMS_ADMIN_SYSTEM_VERSION
 
-
 $env:PWD=$PWD
 
 echo $PWD
@@ -121,7 +127,6 @@ echo "$CMS_FRONTEND_PROXY_VERSION"
 echo "$CMS_FRONTEND_VERSION"
 echo "$CMS_ADMIN_PROXY_VERSION"
 echo "$CMS_ADMIN_SYSTEM_VERSION"
-
 
 echo "**************** Starting Services *****************"
 
