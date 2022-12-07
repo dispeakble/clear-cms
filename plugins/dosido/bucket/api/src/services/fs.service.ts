15b798e8adf5d0015f006e9260c6dfefc7f9c2b0
Ovidiu Alexa <ovidiu.alexa@gmail.com>
2022-12-07 22:56:51 +0000

    bucket fixes and added default logo

--- plugins/dosido/bucket/api/src/services/fs.service.ts
+++ plugins/dosido/bucket/api/src/services/fs.service.ts
@@ -26,7 +26,7 @@ export class FsService {
         return new Observable((observer) => {
             const realPath = this.help.path.realPath({path: params.path});
             try {
-                if (this.help.not.writeable({path: realPath})) {
+                if (this.help.not.readable({path: realPath})) {
                     observer.next({type: 'error', content_length: 0, content_type: "404", message: "not found"});
                     observer.complete();
                     return;
